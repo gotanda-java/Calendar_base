74b1570609e865e7ce67ec53c0b71c64905bb950
7799 <hayashi7799@gmail.com>
2014-05-28 11:21:45 +0900

    履歴表示 日付を選択した時点で表示する仕様にしたが 2重に登録されて表示されるバグを解決していない

--- src/com/example/calendar_base/CalendarActivity.java
+++ src/com/example/calendar_base/CalendarActivity.java
@@ -9,19 +9,22 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class CalendarActivity extends Activity {
+	String date = "";
 
-	private static final String FILE_NAME ="CalendarFile";
-
+	private static final String FILE_NAME = "CalendarFile";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendar);
+
 	}
 
 	@Override
@@ -32,20 +35,24 @@ public class CalendarActivity extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch(item.getItemId()){
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 		case R.id.item1:
 			showDatePickerDialog();
 			return true;
 		case R.id.item2:
-			SharedPreferences preference
-			=getSharedPreferences(FILE_NAME, MODE_PRIVATE);
-
-			String date= preference.getString("DATE", "ありません");
+//			SharedPreferences preference = getSharedPreferences(FILE_NAME,
+//					MODE_PRIVATE);
+//
+//			date += preference.getString("DATE", "ありません")+"\n";
 
 			showDialog(date);
+
+			Log.e("DATE", date);
+
 			return true;
 		}
+
 		return true;
 	}
 
@@ -53,44 +60,52 @@ public class CalendarActivity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		Calendar cal = Calendar.getInstance();
 
-		DatePickerDialog dialog
-		=new DatePickerDialog(CalendarActivity.this, new DatePickerDialog.OnDateSetListener() {
+		DatePickerDialog dialog = new DatePickerDialog(CalendarActivity.this,
+				new DatePickerDialog.OnDateSetListener() {
+
+					public void onDateSet(DatePicker picker, int year,
+							int month, int day) {
+
+						SharedPreferences preference = getSharedPreferences(
+								FILE_NAME, MODE_PRIVATE);
+						// TODO 自動生成されたメソッド・スタブ
+						SharedPreferences.Editor editor = preference.edit();
+						editor.putString("DATE", year + "年" + (month + 1) + "月"
+								+ day + "日");
+						editor.commit();
+
+						date += preference.getString("DATE", "ありません")+"\n";
+				        //レイアウト上のTextViewインスタンスを取得する
+				        TextView showDate = (TextView) findViewById(R.id.tv_date);
+				        //文字列を表示する
+				        showDate.setText(date);
+				        //文字列を追加する
+//				        showDate.append("World!!!");
+
+					}
+				}, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+				cal.get(Calendar.DAY_OF_MONTH));
 
 
-			public void onDateSet(DatePicker picker, int year, int month,
-					int day) {
 
-				SharedPreferences preference
-				=getSharedPreferences(FILE_NAME, MODE_PRIVATE);
-				// TODO 自動生成されたメソッド・スタブ
-				SharedPreferences.Editor editor = preference.edit();
-				editor.putString("DATE",
-						year+"年"+(month +1)+"月"+day+"日");
-				editor.commit();
-			}
-		}
-		,cal.get(Calendar.YEAR)
-		,cal.get(Calendar.MONTH)
-		,cal.get(Calendar.DAY_OF_MONTH)
-		);
 		dialog.show();
 	}
 
-	private void showDialog(String text){
-		AlertDialog.Builder dialog
-		=new AlertDialog.Builder(CalendarActivity.this);
+	private void showDialog(String text) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(
+				CalendarActivity.this);
 		dialog.setTitle("保存結果");
 		dialog.setMessage(text);
 		dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 
-
 			public void onClick(DialogInterface dialog, int whichButton) {
 				// TODO 自動生成されたメソッド・スタブ
 				CalendarActivity.this.setResult(Activity.RESULT_OK);
-		}
-	});
-	dialog.create();
-	dialog.show();
-}
+
+			}
+		});
+		dialog.create();
+		dialog.show();
+	}
 
 }
